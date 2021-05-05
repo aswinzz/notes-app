36f9a8544aeca5a2307854a713b35ff09df9a051
aswinzz <aswinvb.aswin6@gmail.com>
2021-05-05 18:06:08 +0530

    deployment changes

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,3 +1,3 @@
 import { createBrowserHistory } from 'history';
-const history = createBrowserHistory();
+const history = createBrowserHistory({basename: '/notes-app-dev'});
 export default history;
--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -12,7 +12,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/',
+      publicPath: '/notes-app-dev/',
     },
     options.output,
   ), // Merge with env dependent settings
